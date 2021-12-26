bf6389348e4ee74e6451c98fef44610916f55fc5
cerdelen <95369756+cerdelen@users.noreply.github.com>
2021-12-26 16:13:30 +0100

    Update ft_lstadd_back_bonus.c

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -20,11 +20,12 @@
 *	#1. The address of a pointer to the first link of a list.
 *	#2. The address of a pointer to the element to be added to the list.
 */
-
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*ptr;
 
 	ptr = ft_lstlast(*lst);
+	if (!ptr)
+		return (*lst = new);
 	ptr->next = new;
 }
