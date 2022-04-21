e7974a8379d635d8c1cee4928d9606688abe2305
cerdelen <95369756+cerdelen@users.noreply.github.com>
2022-04-21 23:15:40 +0200

    improved ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -25,16 +25,11 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
+	while (*s != (unsigned char) c)
 	{
-		if (s[i] == (unsigned char) c)
-			return ((char *) s + i);
-		i++;
+		if (!*s)
+			return (0);
+		s++;
 	}
-	if ((unsigned char) c == '\0')
-		return ((char *) s + i);
-	return (NULL);
+	return ((char *)s);
 }
