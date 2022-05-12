9f3e6182ddb422003977c9dda50b1be98ab0bf52
Cedric Erdelen <cerdelen@c2s15d88.42wolfsburg.de>
2022-05-12 22:11:45 +0200

    changes to 2d_array_utils

--- two_d_arrays_utils.c
+++ two_d_arrays_utils.c
@@ -6,7 +6,7 @@
 /*   By: cerdelen <cerdelen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 19:47:00 by cerdelen          #+#    #+#             */
-/*   Updated: 2022/05/12 20:34:43 by cerdelen         ###   ########.fr       */
+/*   Updated: 2022/05/12 22:11:22 by cerdelen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ void	print_2d_array(char	**arr, int fd)
 	int	i;
 
 	i = 0;
-	while (arr[i] != NULL)
+	if (arr)
 	{
-		ft_putstr_fd(arr[i], fd);
-		if (arr[i][ft_strlen(arr[i]) - 1] != '\n')
-			ft_putchar_fd('\n', fd); 
-		i++;
+		while (arr[i] != NULL)
+		{
+			ft_putstr_fd(arr[i], fd);
+			if (arr[i][ft_strlen(arr[i]) - 1] != '\n')
+				ft_putchar_fd('\n', fd);
+			i++;
+		}
 	}
 }
 
@@ -33,7 +36,10 @@ void	free_2d_array(char	**arr)
 	int	i;
 
 	i = 0;
-	while (arr[i] != NULL)
-		free(arr[i++]);
-	free(arr);
+	if (arr)
+	{
+		while (arr[i] != NULL)
+			free(arr[i++]);
+		free(arr);
+	}
 }
